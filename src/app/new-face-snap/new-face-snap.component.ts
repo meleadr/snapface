b154dbaed0be5d501bebc90c17222876c0f91f17
meleadr <meledina.adriano@gmail.com>
2023-07-29 16:39:43 +0200

    Premier formulaire reactif

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-new-face-snap',
   templateUrl: './new-face-snap.component.html',
   styleUrls: ['./new-face-snap.component.scss']
 })
-export class NewFaceSnapComponent {
+export class NewFaceSnapComponent implements OnInit{
 
+  snapForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.snapForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null],
+    });
+  }
+
+  onSubmitForm() {
+    console.log(this.snapForm.value);
+  }
 }
