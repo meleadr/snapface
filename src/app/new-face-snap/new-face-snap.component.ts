10e679eeb200d14b591e69943d783ac72e0873cd
meleadr <meledina.adriano@gmail.com>
2023-07-29 16:32:43 +0200

    Setup pour fomulaire reactif

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-face-snap',
+  templateUrl: './new-face-snap.component.html',
+  styleUrls: ['./new-face-snap.component.scss']
+})
+export class NewFaceSnapComponent {
+
+}
