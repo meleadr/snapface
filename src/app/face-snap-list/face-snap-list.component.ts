b60429acaf9496bc13d30bb3840795d6ee271258
meleadr <meledina.adriano@gmail.com>
2023-07-29 17:32:47 +0200

    Utilisation du back avec Http

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,32 +1,20 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FaceSnap} from "../models/face-snap.model";
 import {FaceSnapsService} from "../services/face-snaps.service";
-import {interval, Subject, takeUntil, tap} from "rxjs";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit, OnDestroy {
-  faceSnaps!: FaceSnap[];
-  private destroy$! : Subject<boolean>;
+export class FaceSnapListComponent implements OnInit {
+  faceSnaps$!: Observable<FaceSnap[]>;
 
   constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit():void {
-    this.destroy$ = new Subject<boolean>();
-    this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
-
-    interval(1000).pipe(
-      takeUntil(this.destroy$),
-      tap(console.log)
-    ).subscribe()
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    console.log('FaceSnapListComponent destroyed');
+    this.faceSnaps$ = this.faceSnapsService.getAllFaceSnaps();
   }
 
 }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -1,42 +1,20 @@
 import { Injectable } from '@angular/core';
 import {FaceSnap} from "../models/face-snap.model";
-import {FormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
 export class FaceSnapsService {
 
-  faceSnaps: FaceSnap[] = [
-    {
-      id: 1,
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Paris'
-    },
-    {
-      id: 2,
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 6,
-      location: 'la montagne'
-    },
-    {
-      id: 3,
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    }
-  ];
+  constructor(private http: HttpClient) {
+  }
 
-  getAllFaceSnaps(): FaceSnap[] {
-    return this.faceSnaps;
+  faceSnaps: FaceSnap[] = [];
+
+  getAllFaceSnaps(): Observable<FaceSnap[]> {
+    return this.http.get<FaceSnap[]>('http://localhost:3000/faceSnaps');
   }
 
   getFaceSnapById(id: number): FaceSnap {
