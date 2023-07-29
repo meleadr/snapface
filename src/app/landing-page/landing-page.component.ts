74a51800fe07fe8d26d93f9eb3d8245b4930143f
meleadr <meledina.adriano@gmail.com>
2023-07-29 16:29:46 +0200

    Formulaire angular

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+  userEmail!: string;
 
   constructor(private router: Router) {
   }
+
+  ngOnInit() {
+  }
+
   onContinue() {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm): void{
+    console.log(form.value);
   }
 
 }
